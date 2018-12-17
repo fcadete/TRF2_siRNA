612e355b9846a7bc61c3d68f05b4d68cb4698d91
Filipe Bernardes da Silva Tavares Cadete <fcadete@imm-slurm-p0>
2018-12-17 16:29:54 +0000

    Started analysis of TTAGGG repeats in differentially-expressed genes.

---
+++ scripts/DESeq2_analysis_no_interaction_TTAGGG_repeats.R
@@ -0,0 +1,95 @@
+
+library("DESeq2")
+
+library("tximport")
+library("EnsDb.Hsapiens.v86")
+
+library("tidyverse")
+library("ggrepel")
+library("gridExtra")
+
+library("genefilter")
+library("geneplotter")
+library("VennDiagram")
+
+library("Rsamtools")
+
+setwd("/mnt/TRF2_siRNA/")
+
+pdf("DESeq2_analysis_no_interaction_results/TTAGGG_repeat_analysis.pdf", width = 10)
+
+
+samples <- read_tsv("sample_info.txt")
+samples$condition <- paste(samples$siRNA, samples$TimePoint, sep = "_")
+samples$condition <- relevel(factor(samples$condition), ref = "TRF2_0")
+
+files <- file.path("salmon_on_hg19_output", samples$Filename, "quant.sf")
+names(files) <- samples$Filename
+
+tx2gene <- values(transcripts(EnsDb.Hsapiens.v86))[, c("tx_id", "gene_id")]
+
+txi <- tximport(files, type = "salmon", tx2gene = tx2gene, ignoreTxVersion = TRUE)
+
+ddsTxi <- DESeqDataSetFromTximport(txi,
+                                   colData = samples,
+                                   design = ~ condition)
+
+ddsTxi <- DESeq(ddsTxi)
+
+# Test for genes differentially expressed between timePoints in the TRF2 shRNA data
+res48h <- results(ddsTxi, contrast = c("condition", "TRF2_48", "TRF2_0"))
+res96h <- results(ddsTxi, contrast = c("condition", "TRF2_96", "TRF2_0"))
+
+# Explicitly test for genes with a low LFC in the control timecourse
+res48hControl_lowLFC <- results(ddsTxi,
+                                lfcThreshold = log2(1.25),
+                                altHypothesis = "lessAbs",
+                                contrast = c("condition", "control_0", "control_48"))
+
+res96hControl_lowLFC <- results(ddsTxi,
+                                lfcThreshold = log2(1.25),
+                                altHypothesis = "lessAbs",
+                                contrast = c("condition", "control_0", "control_96"))
+
+
+
+selected_genes_48h <- GenomicFeatures::genes(EnsDb.Hsapiens.v86,
+                                             filter = GeneIdFilter(rownames(
+                                               res48h[which(res48h$padj < 0.1),])))
+
+selected_genes_96h <- GenomicFeatures::genes(EnsDb.Hsapiens.v86,
+                                             filter = GeneIdFilter(rownames(
+                                               res96h[which(res96h$padj < 0.1),])))
+
+selected_genes_48h_control_low_LFC <- GenomicFeatures::genes(EnsDb.Hsapiens.v86,
+                                                     filter = GeneIdFilter(rownames(
+                                                       res48h[which(res48hControl_lowLFC$padj < 0.1),])))
+
+selected_genes_96h_control_low_LFC <- GenomicFeatures::genes(EnsDb.Hsapiens.v86,
+                                                     filter = GeneIdFilter(rownames(
+                                                       res96h[which(res96hControl_lowLFC$padj < 0.1),])))
+
+# Include only the genes that also have a low LFC in the control samples
+selected_genes_48h <- selected_genes_48h[(names(selected_genes_48h) %in% names(selected_genes_48h_control_low_LFC))]
+selected_genes_96h <- selected_genes_96h[(names(selected_genes_96h) %in% names(selected_genes_96h_control_low_LFC))]
+
+Dna <- getGenomeFaFile(EnsDb.Hsapiens.v86)
+
+genes_in_data <- GenomicFeatures::genes(EnsDb.Hsapiens.v86, filter = GeneIdFilter(rownames(res96h)))
+
+genes_in_data <- genes_in_data[seqnames(genes_in_data) %in% seqnames(seqinfo(Dna))]
+
+geneSeqs <- getSeq(Dna, genes_in_data)
+
+gene_telrep_matches <- vmatchPattern("TTAGGG", geneSeqs)
+
+gene_telrep_number_matches <- unlist(lapply(gene_telrep_matches, length))
+
+names(gene_telrep_number_matches) <- names(genes_in_data)
+
+
+gene_telreps <- data_frame(gene_ID = names(genes_in_data),
+                           telrep_matches = gene_telrep_number_matches,
+                           gene_width = width(genes_in_data))
+
+
